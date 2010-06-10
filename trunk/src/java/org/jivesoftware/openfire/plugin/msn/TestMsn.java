17fcadf1703c0e10bc8d91fbcc038bb872a65859
songjiao001 <songjiao001@63087c68-e40e-11de-ae3c-8df03fb1c7b4>
2010-06-10 03:32:51 +0000

    包过滤

---
+++ trunk/src/java/org/jivesoftware/openfire/plugin/msn/TestMsn.java
@@ -0,0 +1,21 @@
+package org.jivesoftware.openfire.plugin.msn;
+
+import net.sf.jml.Email;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: songjiao
+ * Date: 2010-5-23
+ * Time: 14:35:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestMsn {
+    public static void main(String[] args) throws InterruptedException {
+        MsnRobot smn = MsnRobot.getInstance();
+        smn.sendMessage(Email.parseStr("songjiao001@hotmail.com"), "jml������Ϣ2");
+
+
+    }
+}
+
+
